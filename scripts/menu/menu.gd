5a3a2d89fb1d36533958ee6b5cff6315f864d16f
UNSTOP4BLE <bruno.juraski1@gmail.com>
2024-08-02 19:24:20 +0200

    clean code

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -1,98 +1,132 @@
 extends Node3D
 
 @export var camera : Camera3D
+@export var arrowl : AnimatedSprite2D
+@export var arrowr : AnimatedSprite2D
+@export var arrAreaL : Area2D
+@export var arrAreaR : Area2D
+
+var camPos = Vector3(0, 2, 0)
+
+var lastCamX = 0
+var lastCamY = 0
 var cameraX = 0
+var cameraY = 0
+
 var selection = 0
-var inmenu = false
 var changecam = false
+
+var menu_state = 0
+
+var MENU_STATE_HOME = 0
+var MENU_STATE_OPTSELECTED = 1
 
 func _ready():
 	print("test start")   
 	selection = 0
-	inmenu = false
+	menu_state = 0
 	changecam = false
+	
+	camPos = Vector3(0, 2, 0)
+	lastCamX = 0
+	lastCamY = 0
 	cameraX = 0
+	cameraY = 0
 	pass
 	
+func updateCamera(delta):
+	camera.position.x = lerp(float(camera.position.x), float(camPos.x), float(delta * camera.CAMERA_SPEED/2))
+	camera.position.y = lerp(float(camera.position.y), float(camPos.y), float(delta * camera.CAMERA_SPEED/2))
+	camera.position.z = lerp(float(camera.position.z), float(camPos.z), float(delta * camera.CAMERA_SPEED/2))
+	camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(cameraX), float(delta * camera.CAMERA_SPEED/2))
+	camera.rotation_degrees.x = lerp(float(camera.rotation_degrees.x), float(cameraY), float(delta * camera.CAMERA_SPEED/2))
+
 func _process(delta: float) -> void:
-	if $Camera2D/arrowl/Area2D.is_hovered:
-		$Camera2D/arrowl.play("default", 1, false)
-	else:
-		$Camera2D/arrowl.stop()
+	arrow_playAnim(arrowl, arrAreaL);
+	arrow_playAnim(arrowr, arrAreaR);
+
+	#back/accept
+	if Input.is_action_just_pressed("ui_accept"):
+		if (menu_state == MENU_STATE_HOME):
+			menu_state = MENU_STATE_OPTSELECTED;
+		else:
+			menu_state = MENU_STATE_HOME
+		arrow_togglevisible();
 		
-	if $Camera2D/arrowr/Area2D.is_hovered:
-		$Camera2D/arrowr.play("default", 1, false)
-	else:
-		$Camera2D/arrowr.stop()
-	#handle input
-	if !inmenu:	
-		$Camera2D/arrowl.visible = true
-		$Camera2D/arrowr.visible = true
-		if $Camera2D/arrowl/Area2D.justclicked:
+	if (menu_state == MENU_STATE_OPTSELECTED):
+		if selection == 0: #play
+			if !changecam:
+				camera.rotation_degrees.y = 0;
+				cameraX = camera.rotation_degrees.y;
+				lastCamX = cameraX;
+				changecam = true;
+			camPos = Vector3(0, 2, -6.5)
+			cameraX = 90;
+		elif selection == 1: #options			
+			if !changecam:
+				camera.rotation_degrees.y = -90
+				cameraX = camera.rotation_degrees.y;
+				lastCamX = cameraX;
+				changecam = true;
+			camPos = Vector3(6.5, 2, 0)
+			cameraX = -180;
+		elif selection == 2: #exit
+			camPos = Vector3(0, 2, 2.5)
+			cameraY = -20
+		elif selection == 3: #credits
+			if !changecam:
+				camera.rotation_degrees.y = -270
+				cameraX = camera.rotation_degrees.y
+				lastCamX = cameraX;
+				changecam = true
+			camPos = Vector3(-6.5, 2, 0)
+			cameraX = -180;
+			
+	#reset
+	if (menu_state == MENU_STATE_HOME):
+		#options
+		if (cameraX == 0 or cameraX == -360): #play
+			selection = 0;
+		elif (cameraX == -90 or cameraX == 270): #options
+			selection = 1;
+		elif (cameraX == 180 or cameraX == -180): #exit
+			selection = 2;
+		elif (cameraX == 90 or cameraX == -270): #credits
+			selection = 3;
+		
+		cameraX = lastCamX
+		cameraY = lastCamY
+		camPos = Vector3(0, 2, 0)
+		changecam = false;
+		
+		#input
+		if arrAreaL.justclicked or Input.is_action_just_pressed("ui_left"):
 			if (cameraX > 360-90):
 				camera.rotation_degrees.y = 0
 				cameraX = 0
 				selection = 0
 			cameraX += 90
-		elif $Camera2D/arrowr/Area2D.justclicked:
-			if (cameraX < -360+90):
+			lastCamX = cameraX;
+		elif arrAreaR.justclicked or Input.is_action_just_pressed("ui_right"):
+			if (cameraX < -360+90): 
 				camera.rotation_degrees.y = 0
 				cameraX = 0
 				selection = 0
 			cameraX -= 90
-	else:
-		$Camera2D/arrowl.visible = false
-		$Camera2D/arrowr.visible = false
-	#options
-	if cameraX == 0 or cameraX == -360: #play
-		selection = 0
-	elif cameraX == -90 or cameraX == 270: #options
-		selection = 1
-	elif cameraX == 180 or cameraX == -180: #exit
-		selection = 2
-	elif cameraX == 90 or cameraX == -270: #credits
-		selection = 3
-		
-	print(inmenu, " ")
+			lastCamX = cameraX;
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		inmenu = !inmenu
-		
-	if inmenu:
-		if selection == 0: #play
-			if !changecam:
-				camera.rotation_degrees.y = 0
-				cameraX = camera.rotation_degrees.y
-				changecam = true
-			camera.position.z = lerp(float(camera.position.z), float(-6.5), float(delta * camera.CAMERA_SPEED/2))
-			camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(90), float(delta * camera.CAMERA_SPEED/2))
-		elif selection == 1: #options
-			if !changecam:
-				camera.rotation_degrees.y = -90
-				cameraX = camera.rotation_degrees.y
-				changecam = true
-			camera.position.x = lerp(float(camera.position.x), float(6.5), float(delta * camera.CAMERA_SPEED/2))
-			camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(-180), float(delta * camera.CAMERA_SPEED/2))
-		elif selection == 2: #exit
-			camera.position.z = lerp(float(camera.position.z), float(2.5), float(delta * camera.CAMERA_SPEED/2))
-			camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(cameraX), float(delta * camera.CAMERA_SPEED/2))
-			camera.rotation_degrees.x = lerp(float(camera.rotation_degrees.x), float(-80), float(delta * camera.CAMERA_SPEED/2))
-		elif selection == 3: #credits
-			if !changecam:
-				camera.rotation_degrees.y = -270
-				cameraX = camera.rotation_degrees.y
-				changecam = true
-			camera.position.x = lerp(float(camera.position.x), float(-6.5), float(delta * camera.CAMERA_SPEED/2))
-			camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(-180), float(delta * camera.CAMERA_SPEED/2))
-	
-	print(inmenu)
-	#reset
-	if !inmenu:
-		camera.position.z = lerp(float(camera.position.z), float(0), float(delta * camera.CAMERA_SPEED/2))
-		camera.position.x = lerp(float(camera.position.x), float(0), float(delta * camera.CAMERA_SPEED/2))
-		camera.rotation_degrees.y = lerp(float(camera.rotation_degrees.y), float(cameraX), float(delta * camera.CAMERA_SPEED/2))
-		changecam = false;
-	camera.rotation_degrees.x = lerp(float(camera.rotation_degrees.x), float(0),       float(delta * camera.CAMERA_SPEED))
-	
-	print(camera.position.z)
+	#update camera
+	updateCamera(delta)
+
 	pass
+
+#arrow functions 
+func arrow_togglevisible():	
+	arrowl.visible = !arrowl.visible;
+	arrowr.visible = !arrowr.visible;
+
+func arrow_playAnim(arrow : AnimatedSprite2D, area : Area2D):
+	if (area.is_hovered):
+		arrow.play("default", 1, false)
+	else:
+		arrow.stop()
